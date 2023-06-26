e66f10ce74278f3203f8c5c5b52cd4c7e182ec13
BermetM <137826341+BermetM@users.noreply.github.com>
2023-06-26 22:26:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        method(46);
+//   }
+//    static int method(int ... var) {
+//        int sum = 0;
+//        int count = 0;
+//
+//        for (int i = 0; i < var.length; i++) {
+//            sum += var[i];
+//            count++;
+//        }
+//        System.out.println(var.length);
+//        System.out.println(count);
+//        return sum;
+//
+
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+     //   for (int i = 0; i < array.length; i++) {      //   1- var
+       //     System.out.println(array[i]);
+
+     //   for (int i:array){                                // 2- var
+     //       System.out.println(i);
+
+     //   }
+
+//        tabUmno("Bermet","Kymbat");
+//}
+//
+//    static void tabUmno(String...var ){
+//        for (String s : var) {
+//            System.out.println(s+ "  kandai ");
+//
+//        }
+     
+
+
+        }
+
+}
